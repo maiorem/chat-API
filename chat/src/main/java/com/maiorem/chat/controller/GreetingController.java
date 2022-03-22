397570a9991f73fe47439dba0b5d08a22b82911c
Seyoung Hong <maiorem00@gmail.com>
2022-03-22 14:10:30 +0900

    example

---
+++ chat/src/main/java/com/maiorem/chat/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.maiorem.chat.controller;
+
+import com.maiorem.chat.Greeting;
+import com.maiorem.chat.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
