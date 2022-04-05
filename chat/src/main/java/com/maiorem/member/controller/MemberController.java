6d77d4d5a04c4c3f23acda95f6d83e4082f23840
Seyoung Hong <maiorem00@gmail.com>
2022-04-05 20:36:06 +0900

    Update MemberController.java

--- chat/src/main/java/com/maiorem/member/controller/MemberController.java
+++ chat/src/main/java/com/maiorem/member/controller/MemberController.java
@@ -1,4 +1,13 @@
 package com.maiorem.member.controller;
 
+import com.maiorem.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
 }
