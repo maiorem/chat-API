61f8e09d6fd145584b60ecc33150bc351d3389f9
Seyoung Hong <maiorem00@gmail.com>
2022-04-09 23:54:59 +0900

    chat repo

--- chat/src/main/java/com/maiorem/chat/service/ChatService.java
+++ chat/src/main/java/com/maiorem/chat/service/ChatService.java
@@ -1,7 +1,9 @@
 package com.maiorem.chat.service;
 
 import com.maiorem.chat.domain.ChatRoom;
+import com.maiorem.chat.repository.ChatRepository;
 import com.maiorem.member.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,20 +13,28 @@ import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ChatService {
 
     static int roomNumber = 0;
     List<ChatRoom> roomList = new ArrayList<>();
 
+    private final ChatRepository chatRepository;
+
     /**
      * 채팅방 리스트
      */
     public List<ChatRoom> readRoomList() {
-        return roomList
-                .stream()
-                .filter( o-> o.getRoomNumber() == roomNumber)
-                .collect(Collectors.toList());
+        return chatRepository.findAll();
     }
+
+    /**
+     * 채팅방 찾기
+     */
+    public ChatRoom searchChatRoom(Long chatId) {
+        return chatRepository.findById(chatId).get();
+    }
+
 
     /**
      * 채팅방 만들기
